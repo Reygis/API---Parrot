13b4442d35349d70d24b03bca00e72112a51645e
Reygis Meira <reygis@outlook.com>
2022-09-06 20:42:26 -0300

    update seeders

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,7 +4,7 @@ import {checkJwt} from "../middleware/checkJwt"
 
 const router = Router()
 
-router.get("/", checkJwt, new UserController().listAll )
+router.get("/", checkJwt, new UserController().listAll)
 router.get("/:iduser", checkJwt, new UserController().getOneById)
 router.post("/", new UserController().create )
 router.put("/:iduser", checkJwt, new UserController().editUser)
--- src/seeds/MainSeeder.ts
+++ src/seeds/MainSeeder.ts
@@ -1,6 +1,8 @@
 import { DataSource } from 'typeorm'
 import { runSeeder, Seeder, SeederFactoryManager } from 'typeorm-extension'
 import { AdminSeeder } from './AdminSeeder'
+// import { PostSeeder } from './PostSeeder'
+import { UserSeeder } from './UserSeeder'
 
 export class MainSeeder implements Seeder {
 	async run(
@@ -8,5 +10,6 @@ export class MainSeeder implements Seeder {
 		factoryManager: SeederFactoryManager
 	): Promise<void> {
 		await runSeeder(dataSource, AdminSeeder)
+		await runSeeder(dataSource, UserSeeder)
 	}
 }
