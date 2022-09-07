f95998ab0bc5770afb39a0937b155ad02ce59955
Reygis Meira <reygis@outlook.com>
2022-09-06 22:45:42 -0300

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import "reflect-metadata"
 import express from "express"
 import routes from "./routes"
 import {AppDataSource} from "./data-source"
+import cors from "cors"
 
 AppDataSource.initialize().then(()=>{
         const app = express()
         app.use(express.json())
 
+        app.use(cors())
         app.use(routes)
 
         app.listen(3030, ()=>{
