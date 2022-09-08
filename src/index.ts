24f8f18a0de2c2bdd93bc29a0302528c1b67cb14
Reygis Meira <reygis@outlook.com>
2022-09-08 18:38:18 -0300

    updated app.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,11 @@
 import "reflect-metadata"
-import express from "express"
-import routes from "./routes"
 import {AppDataSource} from "./data-source"
-import cors from "cors"
+import app from "./app"
 
 AppDataSource.initialize().then(()=>{
-        const app = express()
-        app.use(express.json())
-
-        app.use(cors())
-        app.use(routes)
-
         app.listen(3030, ()=>{
             console.log("Server started on port 3030")
             console.log("Aperte CTRL+C 2 vezes para parar o servidor")
         })
+        
     }).catch((error)=> console.log(error))
